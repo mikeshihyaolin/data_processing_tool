60c0e4c3b1677ea3e99b92fda2b23f07c80f32a3
Mike Lin <mike.lin@ieee.org>
2018-10-07 01:59:43 -0700

    upload image2video script

---
+++ img2video.py
@@ -0,0 +1,68 @@
+#########################################################################################
+# python img2video.py --input_img_path [image_path] --output_video_path [video_path]    #
+#########################################################################################
+
+import cv2
+import pickle
+import glob
+import argparse
+import numpy as np
+import os, sys
+from os import listdir, makedirs
+import shutil
+import time
+import matplotlib.pyplot as plt
+# matplotlib.use("gtk")
+
+def reset(reset_path):
+    path = reset_path
+    if os.path.isdir(path):
+        shutil.rmtree(path)
+        print("remove existing "+path)
+        makedirs(path)
+        print("create folder: "+path)
+    else:
+        makedirs(path)
+        print("create foder: "+path)
+
+def convert_img_to_video(input_path_, video_path ):
+
+	jpg_list = sorted(glob.glob(input_path_+"/*"))
+
+	if not jpg_list == []:
+		print(input_path_)
+
+		img = []
+		for i,fi in enumerate(jpg_list):
+			print(fi)
+			img.append(cv2.imread(fi))
+			# print(img[i])
+
+		height,width,layers=img[1].shape
+		name = video_path+".mp4"
+		# name = name.replace(" ","")
+		# print(name)
+		fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+		video = cv2.VideoWriter(name, fourcc, 20.0, (width,height))
+
+		for i, fi in enumerate(img):
+			video.write(fi)
+		video.release()
+
+
+if __name__=="__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--input_img_path", type=str)
+	parser.add_argument("--output_video_path", type=str)
+	args = parser.parse_args()
+
+	start = time.time()
+
+	print("input path: "+args.input_img_path)
+	print("output path: "+args.output_video_path)
+
+	convert_img_to_video(args.input_img_path, args.output_video_path)
+
+	end = time.time()
+	elapsed = end - start
+	print ("Time taken: ", elapsed, "seconds.")
